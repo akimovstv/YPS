ff58d4e251712fe18c73035e894fbafb5819f163
Stanislav Akimov <39214862+akimovstv@users.noreply.github.com>
2021-05-05 17:18:24 +0300

    Implement not_empty_name

--- checkers.py
+++ checkers.py
@@ -5,6 +5,7 @@ Additional tools for checking request arguments.
 import datetime
 
 INVALID_DATE_MESSAGE = 'must be valid date in format `YYYY-mm-dd` (e.g. 2020-05-05)'
+INVALID_NAME_MESSAGE = 'must contain at least one non-blank character'
 
 
 def checked_date(s: str) -> str:
@@ -13,3 +14,15 @@ def checked_date(s: str) -> str:
     otherwise raise ValueError.
     """
     return datetime.datetime.strptime(s, '%Y-%m-%d').date().isoformat()
+
+
+def not_empty_name(s: str) -> str:
+    """
+    Return stripped `s`, if it is not empty,
+    otherwise raise ValueError.
+    """
+    s = s.strip()
+    if s:
+        return s
+    else:
+        raise ValueError
